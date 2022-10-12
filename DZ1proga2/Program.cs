d1b2437bc005434222ad83052380ede03df33ee5
nikitaziubin <49118839+nikitaziubin@users.noreply.github.com>
2022-10-12 22:22:15 +0300

    Add files via upload

---
+++ DZ1proga2/Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Ви ввели число {number}"); 
+    }
+
+
+
+
+
+
+}       
